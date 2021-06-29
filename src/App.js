5319c5f01fa56e20aa5a6a569a84f6bfe177c0b8
sujan <asujan205@gmail.com>
2021-06-29 13:35:24 +0545

    hashjing done for small data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+const swear = [
+'पपप',
+'मु',
+'फक',
+'मक',
+'ल',
+'तुरी',
+'थ',
+'न',
+
+]
 
 function App() {
+const[key,setKey]=useState([]);
+const[hasharray,setArray]=useState([]);
+const[swearwords,setSwear]=useState(swear)
+/*useEffect(()=>{
+  swear.map(data=>
+  // setKey(data.toString().length)
+
+key=data.length
+
+    )
+  
+},[]) */
+const[size,setSize]=useState(swearwords.length);
+
+const hashkey=(key)=>{
+  
+return key.length %size;
+
+}
+const  set=(value)=>{
+
+    let index = hashkey(value);
+   console.log(index)
+
+    if(!hasharray[index]){
+      hasharray[index] = [ ];
+    }
+
+    hasharray[index].push([value])
+console.log(hasharray)
+    return index
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {set(swearwords[0])}
+      {set(swearwords[2])}
+     {set("ववव")}
+
+
     </div>
   );
+
 }
 
 export default App;
